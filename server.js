71b4c30ae4396754c54dfdb8a46f455c89ce307f
Joy Harrington <joysyk23@gmail.com>
2024-10-09 09:33:44 -0500

    cleaning up

--- server.js
+++ server.js
@@ -10,32 +10,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// /notes endpoints
-const notes = require("./data/notes");
-
-app.get("/notes", (req, res) => {
-  res.json(notes);
-});
-
-app.get("/notes/:id", (req, res, next) => {
-  const { id } = req.params;
-  const note = notes.find((n) => n.id === +id);
-  if (note) {
-    res.json(note);
-  } else {
-    next({ status: 404, message: `Note with id ${id} does not exist.` });
-  }
-});
-
-app.post("/notes", (req, res, next) => {
-  const { text } = req.body;
-  if (text) {
-    notes.push({ id: notes.length + 1, text });
-    res.status(201).json(notes.at(-1));
-  } else {
-    next({ status: 400, message: `New note must have text.` });
-  }
-});
+app.use("notes", require("./api/notes"));
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
